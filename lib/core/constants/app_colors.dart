991db037ef8647cd3ce3dcff070414794299bac5
Marwa Eltayeb <marwa_eltayeb@yahoo.com>
2026-07-26 01:07:05 +0200

    Add AppColors and AppStrings constants

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFFFF5F5);
+  static const Color primary = Colors.deepOrange;
+  static const Color cardBackground = Colors.white;
+  static const Color textPrimary = Colors.black;
+  static const Color textSecondary = Colors.grey;
+  static const Color rating = Colors.amber;
+  static Color imagePlaceholder = Colors.grey[200]!;
+}
---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,4 @@
+class AppStrings {
+  static const String appTitle = 'Recipe Hub';
+  static const String noRecipesFound = 'No Recipes Yet';
+}
